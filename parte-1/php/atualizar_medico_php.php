a7ea6d2440466a5cba52b5e0d63155df379b51f3
Mafezuda <moreira.mafe@gmail.com>
2024-04-04 15:55:31 -0700

    .

--- parte-1/php/atualizar_medico_php.php
+++ parte-1/php/atualizar_medico_php.php
@@ -4,40 +4,32 @@ include("base.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST['atualizar'])){
-        $crm = $_POST['crm'];
+        $crm = $_POST['CRM'];
         $nome = $_POST['nome'];
-        $cpf = $_POST['cpf'];
         $data_nascimento = $_POST['data'];
-        $especialidade = $_POST['especialidade'];
         $senha = $_POST['senha'];
-        $confirmaSenha = $_POST['confirmaSenha'];
 
-        if (strlen($senha) < 6 || strlen($senha) > 30) {
-            echo "<div class='message-box error'><span>A senha deve ter entre 6 e 30 caracteres!</span></div>";
-        } elseif (!preg_match("/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}-?[0-9]{2}$/", $cpf)) {
-            echo "<div class='message-box error'><span>CPF inválido! Digite no formato xxxxxxxxxxx ou xxx.xxx.xxx-xx</span></div>";
-        } else {
-            $sql = "SELECT medico_cpf FROM medico WHERE crm = '$crm'";
-            $resultado = $conn->query($sql);
-            if ($resultado->num_rows == 1) {
-                $row = $resultado->fetch_assoc();
-                $medico_cpf = $row['medico_cpf'];
+        $sql_medico = "SELECT medico_cpf, especialidade_id FROM medico WHERE crm = '$crm'";
+        $resultado_medico = $conn->query($sql_medico);
 
-                $sql_update_usuario = "UPDATE usuario SET nome = '$nome', data_nascimento = '$data_nascimento', senha = '$senha' WHERE cpf = '$cpf'";
-                $sql_update_medico = "UPDATE medico SET medico_cpf = '$cpf', especialidade_id = '$especialidade' WHERE crm = '$crm'";
-                
-                if ($conn->query($sql_update_usuario) === TRUE && $conn->query($sql_update_medico) === TRUE) {
-                    echo "<div class='message-box success'><span>Dados do médico atualizados com sucesso!</span></div>";
-                } else {
-                    echo "<div class='message-box error'><span>Erro ao atualizar os dados do médico: " . $conn->error . "</span></div>";
-                }
+        if ($resultado_medico->num_rows == 1) {
+            $row_medico = $resultado_medico->fetch_assoc();
+            $medico_cpf = $row_medico['medico_cpf'];
+            $especialidade_id = $row_medico['especialidade_id'];
+
+            $sql_update_usuario = "UPDATE usuario SET nome = '$nome', data_nascimento = '$data_nascimento', senha = '$senha' WHERE cpf = '$medico_cpf'";
+            if ($conn->query($sql_update_usuario) === TRUE) {
+                echo "<div class='message-box success'><span>Dados do médico atualizados com sucesso!</span></div>";
             } else {
-                echo "<div class='message-box error'><span>CRM não encontrado!</span></div>";
+                echo "<div class='message-box error'><span>Erro ao atualizar os dados do médico: " . $conn->error . "</span></div>";
             }
+        } else {
+            echo "<div class='message-box error'><span>CRM não encontrado!</span></div>";
         }
     } elseif(isset($_POST['excluir'])){
-        $crm = $_POST['crm'];
-        
+
+        $crm = $_POST['CRM'];
+
         $sql_delete_medico = "DELETE FROM medico WHERE crm = '$crm'";
         if ($conn->query($sql_delete_medico) === TRUE) {
             echo "<div class='message-box success'><span>Médico excluído com sucesso!</span></div>";
