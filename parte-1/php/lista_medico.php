db1dc287adfd31218994d03afceb9c1d405c8b1b
Maria Fernanda <moreira.mafe@gmail.com>
2024-04-05 08:09:32 -0300

    .

--- parte-1/php/lista_medico.php
+++ parte-1/php/lista_medico.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/lista_paciente.css">
+    <link rel="stylesheet" href="../css/agenda_recepcionista.css">
     <title>Lista de Médicos</title>
 </head>
 
