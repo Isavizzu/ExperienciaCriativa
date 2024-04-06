8b2a0303c2b20d69d4279e48c6e89750a300639d
Mafezuda <moreira.mafe@gmail.com>
2024-04-06 10:58:27 -0700

    ,

--- parte-1/php/cadastromed.php
+++ parte-1/php/cadastromed.php
@@ -1,15 +1,8 @@
 @ -1,121 +0,0 @@
 <?php
-include("base.php");
-include("conexao.php");
+    include("base.php");
+    include("conexao.php");
 
-function verifica_crm($crm){
-    global $conn;
-    $sql = "SELECT crm FROM medico WHERE crm = '$crm'";
-    $resultado = $conn->query($sql);
-    $row = $resultado->fetch_assoc();
-    return ($row != null);
-} 
 ?>
 
 <!DOCTYPE html>
@@ -19,37 +12,31 @@ function verifica_crm($crm){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Médico</title>
     <link rel="stylesheet" href="../css/cadastro.css">
-    <script>
-        function showPassword() {
-            var senhaInput = document.getElementById("senha");
-            if (senhaInput.type === "password") {
-                senhaInput.type = "text";
-            } else {
-                senhaInput.type = "password";
-            }
-        }
-    </script>
 </head>
 <body>
     <br>
     <h1>Formulário de Cadastro de Médico</h1>
     <br>
+
     <section class="caixa">
 
-        <form class="form" id="form" name="form" action="criar_cadastro_med.php" method="post">
+        <form class="form" id="form" name="form" method="post">
 
             <div class="input-box">
                 <label>Nome completo</label>
                 <input type="text" id="nome" name="nome" placeholder="Digite o nome completo" required="">
             </div>
+
             <div class="input-box">
                 <label>CPF</label>
                 <input type="text" id="cpf" name="cpf" placeholder="Digite o CPF no formato xxxxxxxxxxx ou xxx.xxx.xxx-xx" required="">
             </div>
+
             <div class="input-box">
                 <label>Data de Nascimento</label>
                 <input type="date" id='data' name="data" placeholder="Digite a data de nascimento">
             </div>
+
             <div class="input-box">
                 <label>CRM</label>
                 <input type="text" id="crm" name="crm" placeholder="Digite o CRM" required="">
@@ -75,11 +62,8 @@ function verifica_crm($crm){
                 <label>Confirme a senha</label>
                 <input type="password" id="confirmaSenha" name="confirmaSenha" placeholder="Confirme sua senha" required="">
             </div>
-        </form>
-    </section>
-
+            <br>
     <?php
-
         if(isset($_POST['Cadastrar'])) {
             botao_cadastrar();
         }
@@ -99,24 +83,53 @@ function verifica_crm($crm){
 
         function botao_cadastrar(){
 
+            global $conn;
             $cpf = $_POST['cpf'];
             $Nome = $_POST['nome'];
             $dat = $_POST['data'];
             $crm = $_POST['crm'];
-            $especialidade = $_POST['especialidade'];
             $Senha = $_POST['Senha'];
             $confirmasenha = $_POST['confirmaSenha'];
+            $especialidade = $_POST['especialidade'];
+
+            $datformat = date('Y-m-d',strtotime($dat));
 
             if(verifica_cpf($cpf) == true){
                 echo "<section class='section_invalido'><p>Esse CPF já foi cadastrado anteriormente!</p></section>";
             }
+            else if(empty($crm) || !preg_match('/^[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}$/', $crm)){
+                echo "<section class='section_invalido'><p>Digite um CRM válido!</p></section>";
+            }
             else if($Senha != $confirmasenha){
                 echo "<section class='section_invalido'><p>As senhas não correspondem!</p></section>";
             }
+            else if(!preg_match('/^[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}$/', $cpf)){
+                echo "<section class='section_invalido'><p>Digite um CPF válido!</p></section>";
+            }
+            else if(!preg_match('/^.{6,30}$/', $Senha)){
+                echo "<section class='section_invalido'><p>A senha precisa ter no mínimo 6 caracteres!</p></section>";
+            
+            else{
+                    $sqlInsertUsuario = "INSERT INTO usuario(cpf, nome, data_nascimento, senha) VALUES ('$cpf', '$Nome', '$datformat', '$Senha')";
+                    $sqlInsertMedico = "INSERT INTO medico(crm, medico_cpf, especialidade_id) VALUES ('$crm', '$cpf', '$especialidade')";
+                    $conn->query($sqlInsertUsuario);
+                    $conn->query($sqlInsertMedico);
+                
+                echo '<meta http-equiv="refresh" content="0; URL=cadastro_med_php.php?val=1">';
+            }
+            
+          }
         }
-        ?>
+          
 
-        <input type="submit" id="Enviar" class="cadbot" name="Cadastrar" value="Cadastrar" onclick="confirm()">
+      ?>
 
-</body>
+      <input type="submit" id="Enviar" class="cadbot" name="Cadastrar" value="Cadastrar" onclick="confirm()">
+  
+    </form>
+
+</section>
+
+</div>
+</body> 
 </html>
